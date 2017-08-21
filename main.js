c9c897f3a31ab402d2589d3d3c3c6f4884f26522
Danielle Carrick <dacarrick@gmail.com>
2017-08-20 21:33:58 -0400

    basic and buggy zooming

--- main.js
+++ main.js
@@ -23,6 +23,17 @@ var svg = d3.select("body").append("svg")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+var container = svg.append("g");
+
+var zoom = d3.zoom()
+    .scaleExtent([1, 40])
+    //.translateExtent([[-100, -100], [width + 90, height + 100]])
+    .on("zoom", zoomed);
+
+var tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
 // Get the data
 d3.csv("tweets.csv", function(error, data) {
   if (error) throw error;
@@ -36,16 +47,13 @@ d3.csv("tweets.csv", function(error, data) {
 
   // Scale the range of the data
   x.domain(d3.extent(data, function(d) { return d.created_at; }));
-  y.domain([0, d3.max(data, function(d) { return d.retweet_count; })]);
+ // y.domain([0, d3.max(data, function(d) { return d.retweet_count; })]);
+  y.domain([0, 100000]);
   r.domain(d3.extent(data, function(d) { return d.favorite_count; }));
   color.domain([d3.max(data, function(d) { return d.favorite_count; }), d3.min(data, function(d) { return d.favorite_count; })]);
 
-var div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
   // Add the scatterplot
-  svg.selectAll("dot")
+  container.selectAll("dot")
       .data(data)
     .enter().append("circle")
       .attr("r", function(d) { return r(d.favorite_count); })
@@ -53,26 +61,57 @@ var div = d3.select("body").append("div")
       .attr("cy", function(d) { return y(d.retweet_count); })
       .style("fill", function(d) {return color(d.favorite_count); })
       .on("mouseover", function(d) {
-       div.transition()
+       tooltip.transition()
          .duration(200)
          .style("opacity", .9);
-       div.html(d.created_at + "<br/>" + d.text)
+       tooltip.html(d.created_at + "<br/>" + d.text + "<br/>" + "Retweets: " + d.retweet_count + "<br/>" + "Favorites: " + d.favorite_count)
          .style("left", (d3.event.pageX) + "px")
          .style("top", (d3.event.pageY - 28) + "px");
        })
      .on("mouseout", function(d) {
-       div.transition()
+       tooltip.transition()
          .duration(500)
          .style("opacity", 0);
        });
 
-  // Add the X Axis
-  svg.append("g")
+/*  // Add the X Axis
+  container.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
   // Add the Y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
+  container.append("g")
+      .call(d3.axisLeft(y));*/
 
 });
+
+var xAxis = d3.axisBottom(x);
+
+var yAxis = d3.axisLeft(y);
+/*    .ticks(10)
+    .tickSize(width)
+    .tickPadding(8 - width);*/
+
+/*var view = svg.append("rect")
+    .attr("class", "view")
+    .attr("x", 0.5)
+    .attr("y", 0.5)
+    .attr("width", width - 1)
+    .attr("height", height - 1);*/
+
+var gX = svg.append("g")
+    .attr("class", "axis axis--x")
+    .call(xAxis)
+    .attr("transform", "translate(0," + height + ")");
+
+var gY = svg.append("g")
+    .attr("class", "axis axis--y")
+    .call(yAxis);
+
+svg.call(zoom);
+
+function zoomed() {
+  container.attr("transform", d3.event.transform);
+  gX.call(xAxis.scale(d3.event.transform.rescaleX(x)));
+  gY.call(yAxis.scale(d3.event.transform.rescaleY(y)));
+}
